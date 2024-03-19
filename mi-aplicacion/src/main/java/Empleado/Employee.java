743be58e58edd9eeb537f549ca158761a60511b9
Keiner <khuresc2805@g.educaand.es>
2024-03-19 20:20:50 +0100

    Creación clase employee

---
+++ mi-aplicacion/src/main/java/Empleado/Employee.java
@@ -0,0 +1,25 @@
+package Empleado;
+
+public class Employee {
+	String nombre;
+	Integer anios;
+	public Employee(String nombre, Integer anios) {
+		this.nombre = nombre;
+		this.anios = anios;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getAnios() {
+		return anios;
+	}
+	public void setAnios(Integer anios) {
+		this.anios = anios;
+	}
+	
+	
+	
+}
